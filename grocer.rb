1c094f1e041d5b346e155ee1650611adb3ce231e
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 15:05:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,7 +42,7 @@ def apply_clearance(cart)
   # code here
   cart.keys.each do |item|
     if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price] * 0.8).round
+      cart[item][:price] = (cart[item][:price] * 0.8).round(2)
     end
   end
   cart
