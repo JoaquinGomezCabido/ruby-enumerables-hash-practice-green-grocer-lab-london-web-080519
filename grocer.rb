d5c132336f2db8ce9bd8344436cbbe4262605f62
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 14:56:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,22 +18,25 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
-
-  cart.map do |element|
-    if cart.[coupons[:item]][:count] > coupons[:num]
-      cart.[coupons[:item]][:count] -= coupons[:num]
-      cart.["#{coupon[:item]} W/COUPON"] = {
-        price: coupon[:cost] / coupon[:num]
-        clearance: true
-        count: coupons[:num]
-      }
-    elsif cart.[coupons[:item]][:count] == coupons[:num]
-      cart.[coupons[:item]][:price] = coupon[:cost] / coupon[:num]
-    else
-      #nothing
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
     end
   end
   cart
+end
 end
 
 def apply_clearance(cart)
